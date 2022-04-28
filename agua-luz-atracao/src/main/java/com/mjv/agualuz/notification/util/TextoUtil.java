ac187402a358b9031a5c8849722b987deb541963
Gleyson Sampaio <glysns@hotmail.com>
2022-04-28 08:53:34 -0300

    teste

--- agua-luz-atracao/src/main/java/com/mjv/agualuz/notification/util/TextoUtil.java
+++ agua-luz-atracao/src/main/java/com/mjv/agualuz/notification/util/TextoUtil.java
@@ -1,14 +1,31 @@
 package com.mjv.agualuz.notification.util;
 
 public class TextoUtil {
-	public static String cortar(String texto, int tamanhoMaximo) {
+	public static String cortar(String textoOriginal, int tamanhoMaximo) {
 		//aqui já podemos ter um erro
-		String novoTexto = texto.substring(0, tamanhoMaximo);
+		String novoTexto = textoOriginal.substring(0, tamanhoMaximo);
 		return novoTexto; 
 	}
-	public static String completar(String texto, int tamanhoMaximo) {
+	public static String completar(String textoOriginal, int tamanhoMaximo) {
 		String formato = "%-"+tamanhoMaximo;
-		String novoTexto = String.format(formato, texto);
+		String novoTexto = String.format(formato, textoOriginal);
 		return novoTexto; 
+	}
+	public static String ajustar(String textoOriginal, int tamanhoMaximo) {
+		String textoAjustado = cortar(textoOriginal, tamanhoMaximo);
+		textoAjustado = completar(textoAjustado, tamanhoMaximo);
+		return textoAjustado;
+	}
+	public static String zeroEsquerda(String textoOriginal, int tamanhoMaximo) {
+		//removendo caratecteres especiais no texto Original
+		textoOriginal = textoOriginal.replaceAll("\\D", "");
+		//convertendo o texto em numero para formatar com zero a esquerda
+		Long numero = Long.valueOf(textoOriginal);
+		//definindo o formato
+		String formato = "%0"+tamanhoMaximo+"d";
+		//formatando
+		String textoAjustado = String.format(formato, numero);
+		return textoAjustado;
+		
 	}
 }
